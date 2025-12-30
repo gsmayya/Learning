1bdf1bf80d91754f1598f259fd216956a850c165
Gururaj Seetharama <gsmayya@gmail.com>
2025-12-30 13:03:29 +0530

    Removing this to new place

--- jlox/generateAst.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo "Building the tool" 
-./gradlew build 
-echo "Generating the class" 
-java -cp build/classes/java/main com.craftinginterpreters.tool.GenerateAst app/src/main/java/com/craftinginterpreters/lox/
